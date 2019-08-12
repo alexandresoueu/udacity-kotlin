2ed3d95df8a3a58f71cdaccf8f0b055bdc6fe72b
Alexandre DEV <calexandredev@gmail.com>
2019-08-12 18:37:16 -0300

    Aquarium Manager

---
+++ src/Functions/AMS.kt
@@ -0,0 +1,15 @@
+package Functions
+
+fun main(args: Array<String>) {
+    println("Hello ${args[0]}")
+
+    val isUnit = println("This is an expression!")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "You're ${if (temperature > 50) "fried" else "safe"} fish"
+    println(message)
+}
