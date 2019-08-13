29aa3042de9110beb895ac1082f10f446b69f302
Alexandre DEV <calexandredev@gmail.com>
2019-08-13 16:48:46 -0300

    Add Change Water to Manage Aquarium

--- src/Functions/RandomDay.kt
+++ src/Functions/RandomDay.kt
@@ -7,11 +7,27 @@ fun main(args: Array<String>) {
     feedTheFish()
 }
 
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return true
+}
+
+fun shouldChangeWaterTwo(temperature: Int = 22, dirty: Int = 20, day: String) {
+}
+
 fun feedTheFish() {
     val day = randomDay()
     val food = randomFood(day)
 
     println("Today is ${day} and the fish eat ${food}")
+
+    shouldChangeWater(day, 20, 50)
+    shouldChangeWater(day)
+    shouldChangeWater(day, dirty = 50)
+    shouldChangeWaterTwo(day = "Monday")
+
+    if (shouldChangeWater(day)) {
+        println("Change the water")
+    }
 }
 
 fun randomDay(): String {
