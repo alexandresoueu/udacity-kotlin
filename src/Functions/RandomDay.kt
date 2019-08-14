415aea115f6a14bcddff780a22a2c295c1e4bb3f
Alexandre DEV <calexandredev@gmail.com>
2019-08-14 09:33:32 -0300

    Add Temperature and Dirty to Manage Aquarium

--- src/Functions/RandomDay.kt
+++ src/Functions/RandomDay.kt
@@ -5,11 +5,38 @@ import java.util.*
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}")
     feedTheFish()
+
+    var bubbles = 0
+    while (bubbles < 50) {
+        bubbles++
+    }
+
+    repeat(2) {
+        println("A fish is swimming!")
+    }
 }
 
-fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return true
+fun getDirtySensorReading() = 20
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = getDirtySensorReading()): Boolean {
+
+//    val isTooHot = temperature > 30
+//    val isDirty = dirty > 30
+//    val isSunday = day == "Sunday"
+
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
 }
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 fun shouldChangeWaterTwo(temperature: Int = 22, dirty: Int = 20, day: String) {
 }
