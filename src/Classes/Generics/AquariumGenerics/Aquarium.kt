d2a6534dfd4a87b0898db3e62afa364865f1eaa0
Alexandre DEV <calexandredev@gmail.com>
2019-08-21 23:52:04 -0300

    Exercise about Material

--- src/Classes/Generics/AquariumGenerics/Aquarium.kt
+++ src/Classes/Generics/AquariumGenerics/Aquarium.kt
@@ -15,37 +15,28 @@ class TapWater : WaterSupply(true) {
 class FishStoreWater : WaterSupply(false)
 
 class LakeWater : WaterSupply(true) {
-    fun filter() {
-        needsProcessed = false
-    }
+    fun filter() = apply { needsProcessed = false }
 }
 
 class Aquarium<out T : WaterSupply>(val waterSupply: T) {
 
-    fun addWater(cleaner: Cleaner<T>) {
-        if (waterSupply.needsProcessed) {
-            cleaner.clean(waterSupply)
-        }
+    fun addWater() {
+        check(!waterSupply.needsProcessed) { "water supply needs processed" }
         println("adding water from $waterSupply")
     }
+
 }
 
-interface Cleaner<in T : WaterSupply> {
-    fun clean(waterSupply: T)
+inline fun <reified R : WaterSupply> Aquarium<WaterSupply>.hasWaterSupplyOfType() = waterSupply is R
+
+fun <T : WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println("aquarium water is clean: ${aquarium.waterSupply.needsProcessed}")
 }
 
-class TapWaterCleaner : Cleaner<TapWater> {
-    override fun clean(waterSupply: TapWater) {
-        waterSupply.addChemicalCleaners()
-    }
-}
-
-fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+inline fun <reified T : WaterSupply> WaterSupply.isOfType() = this is T
 
 fun genericExample() {
-    val cleaner = TapWaterCleaner()
     val aquarium = Aquarium(TapWater())
-    aquarium.addWater(cleaner)
-
-
+    aquarium.hasWaterSupplyOfType<TapWater>()
+    aquarium.waterSupply.isOfType<LakeWater>()
 }
---
+++ src/Classes/Generics/ExerciseMaterial.kt
@@ -0,0 +1,14 @@
+package Classes.Generics
+
+/*Practice Time
+
+Create a generic function for type BaseBuildingMaterial and call it isSmallBuilding, which takes a Building with a building material T as an argument. If the materials needed are less than 500, print "small building", otherwise, print "large building".
+
+Note: For this function, IntelliJ recommends not to inline the function. Generally, when you create a generic function, follow the IDE's recommendation about inlining.
+*/
+
+fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>) {
+    if (building.actualMaterialsNeeded < 500) println("Small building")
+    else println("large building")
+}
+//TODO: isSmallBuilding(Building(Brick()))
